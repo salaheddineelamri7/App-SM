da00acc686bb9bd58af64dcdad9f60e3c0bd1a58
elamri <salaheddine.elamri.iga@gmail.com>
2024-02-06 23:40:30 +0100

    First commit

---
+++ src/main/java/com/salah/AppMS/AppMsApplication.java
@@ -0,0 +1,13 @@
+package com.salah.AppMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salah/AppMS/AppMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salah.AppMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
